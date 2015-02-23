c64b497a7ad35db7170980e3e4f4ab5f9fa0a391
Matthew Spangenberg <mspangenberg@stumbleupon.com>
2015-02-23 01:59:18 -0800

    Updates

--- js/box-layout/boxlayout.js
+++ js/box-layout/boxlayout.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -43,9 +43,9 @@ var Boxlayout = (function() {
 	}
 
 	function initEvents() {
-		
+
 		$sections.each( function() {
-			
+
 			var $section = $( this );
 
 			// expand the clicked section and scale down the others
@@ -53,11 +53,11 @@ var Boxlayout = (function() {
 
 				if( !$section.data( 'open' ) ) {
 					$section.data( 'open', true ).addClass( 'bl-expand bl-expand-top' );
-					$el.addClass( 'bl-expand-item' );	
+					$el.addClass( 'bl-expand-item' );
 				}
 
 			} ).find( 'span.bl-icon-close' ).on( 'click', function() {
-				
+
 				// close the expanded section and scale up the others
 				$section.data( 'open', false ).removeClass( 'bl-expand' ).on( transEndEventName, function( event ) {
 					if( !$( event.target ).is( 'section' ) ) return false;
@@ -69,68 +69,10 @@ var Boxlayout = (function() {
 				}
 
 				$el.removeClass( 'bl-expand-item' );
-				
+
 				return false;
 
 			} );
-
-		} );
-
-		// clicking on a work item: the current section scales down and the respective work panel slides up
-		$workItems.on( 'click', function( event ) {
-
-			// scale down main section
-			$sectionWork.addClass( 'bl-scale-down' );
-
-			// show panel for this work item
-			$workPanelsContainer.addClass( 'bl-panel-items-show' );
-
-			var $panel = $workPanelsContainer.find("[data-panel='" + $( this ).data( 'panel' ) + "']");
-			currentWorkPanel = $panel.index();
-			$panel.addClass( 'bl-show-work' );
-
-			return false;
-
-		} );
-
-		// navigating the work items: current work panel scales down and the next work panel slides up
-		$nextWorkItem.on( 'click', function( event ) {
-			
-			if( isAnimating ) {
-				return false;
-			}
-			isAnimating = true;
-
-			var $currentPanel = $workPanels.eq( currentWorkPanel );
-			currentWorkPanel = currentWorkPanel < totalWorkPanels - 1 ? currentWorkPanel + 1 : 0;
-			var $nextPanel = $workPanels.eq( currentWorkPanel );
-
-			$currentPanel.removeClass( 'bl-show-work' ).addClass( 'bl-hide-current-work' ).on( transEndEventName, function( event ) {
-				if( !$( event.target ).is( 'div' ) ) return false;
-				$( this ).off( transEndEventName ).removeClass( 'bl-hide-current-work' );
-				isAnimating = false;
-			} );
-
-			if( !supportTransitions ) {
-				$currentPanel.removeClass( 'bl-hide-current-work' );
-				isAnimating = false;
-			}
-			
-			$nextPanel.addClass( 'bl-show-work' );
-
-			return false;
-
-		} );
-
-		// clicking the work panels close button: the current work panel slides down and the section scales up again
-		$closeWorkItem.on( 'click', function( event ) {
-
-			// scale up main section
-			$sectionWork.removeClass( 'bl-scale-down' );
-			$workPanelsContainer.removeClass( 'bl-panel-items-show' );
-			$workPanels.eq( currentWorkPanel ).removeClass( 'bl-show-work' );
-			
-			return false;
 
 		} );
 
